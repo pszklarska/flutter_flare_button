5a48b5cd2a3bdf5f4e4bb94ac6f6e7bab0a75055
pszklarska <pszklarska@gmail.com>
2019-04-22 19:30:49 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlareButtonApp());
+
+class FlareButtonApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
