2cf58870b62356bf692f0226d9abe76716bb429f
pszklarska <pszklarska@gmail.com>
2019-04-22 19:52:55 +0200

    Adding Flare button animation file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(FlareButtonApp());
 
-class FlareButtonApp extends StatelessWidget {
+class FlareButtonApp extends StatefulWidget {
+  @override
+  _FlareButtonAppState createState() => _FlareButtonAppState();
+}
+
+class _FlareButtonAppState extends State<FlareButtonApp> {
+  String _animation = 'Loading';
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        body: FlareActor(
+          "assets/button.flr",
+          animation: _animation,
+        ),
+      ),
+    );
   }
 }
